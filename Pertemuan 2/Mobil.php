c3a2c1e7887f66296da7cd5bf19f87eb0dcfc267
WildanPrasetyoK193040146 <193040146.wildan@mail.unpas.ac.id>
2021-09-21 11:29:25 +0700

    Menyelesaikan Video 3 mengenai Property & Object 1

---
+++ Pertemuan 2/Mobil.php
@@ -0,0 +1,21 @@
+<?php
+
+class Mobil {
+    public $nama,
+    public $merek,
+    public $warna,
+    public $kecepatanMaksimal,
+    public $jumlahPenumpang;
+
+    public function tambahKecepatan() {
+
+    }
+
+    public function kurangKecepatan() {
+
+    }
+
+    public function gantiTransmisi() {
+        
+    }
+}
